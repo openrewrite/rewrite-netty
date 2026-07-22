fb3176be5a0f9f2d751eadb271233e135eca526c
Tim te Beek <tim@moderne.io>
2026-07-22 16:13:18 +0200

    Change projectId from 'rewrite' to 'openrewrite'

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 develocity {
     server = "https://community.develocity.cloud/"
-    projectId = "rewrite"
+    projectId = "openrewrite"
 
     val isCiServer = System.getenv("CI")?.equals("true") ?: false
     val accessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")
